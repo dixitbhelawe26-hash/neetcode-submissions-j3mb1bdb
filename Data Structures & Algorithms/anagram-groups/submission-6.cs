d43bb5e23dec86d27357276276a44c2255f74ac5
dixitbhelawe26-hash <dixitbhelawe26@gmail.com>
2026-04-10 00:06:30 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.cs
@@ -0,0 +1,36 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) 
+    {
+        var dict = new Dictionary<string, List<string>>();
+
+        foreach (var s in strs)
+        {
+            int[] count = new int[26];
+
+            foreach (char c in s)
+            {
+                count[c - 'a']++;
+            }
+
+            var sb = new StringBuilder();
+
+            foreach (int i in count)
+            {
+                sb.Append(i).Append('#');
+            }
+
+            string key = sb.ToString();
+
+            if (!dict.TryGetValue(key, out var list))
+            {
+                list = new List<string>();
+                dict[key] = list;
+            }
+
+            list.Add(s);
+        }
+
+        return dict.Values.ToList();
+    
+    }
+}
