2ad22aa56944628ade5ccb3506878fe26e167fe6
dixitbhelawe26-hash <dixitbhelawe26@gmail.com>
2026-04-10 11:32:01 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cs
@@ -0,0 +1,31 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+
+StringBuilder sb = new StringBuilder();
+
+foreach (char c in s)
+{
+    if (char.IsLetterOrDigit(c))
+    {
+        sb.Append(char.ToLower(c)); // optional (useful for palindrome)
+    }
+}
+
+s = sb.ToString();
+int left = 0, right = s.Length - 1;
+
+while (left < right)
+{
+    if (s[left] != s[right])
+    {
+        return false;
+    }
+
+    left++;
+    right--;
+}
+
+return true;
+        
+}
+}
